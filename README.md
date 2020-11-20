bfe73095553be0007f8bffbd1c11add8d62e1e41
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2020-11-20 23:49:46 +0200

    poc of writing text

---
+++ README.md
@@ -0,0 +1,6 @@
+WARNING: This is a userspace proof of concept for a WIP firmware change. Everything will change. Might break your hardware.
+
+Usage:
+
+1. Clone repo and install Rust
+2. `cargo run AWE` to display `AWE` on the UHK display
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,16 @@
 fn main() {
+    let text = std::env::args().nth(1).unwrap();
+    assert!(text.len() == 3);
+
+    let mut command_data = [0x21, 0, 0, 0];
+    command_data[1..].clone_from_slice(&text.as_bytes());
+
     let mut handle = rusb::open_device_with_vid_pid(0x1d50, 0x6122).unwrap();
 
-    // let device = handle.device();
-    // for interface in device.active_config_descriptor().unwrap().interfaces() {
-    //     for idesc in interface.descriptors() {
-    //         for edesc in idesc.endpoint_descriptors() {
-    //             if edesc.address() == 0x83 {
-    //                 println!("{:#x?}", idesc);
-    //                 println!("{:#x?}", edesc.transfer_type());
-    //             }
-    //         }
-    //     }
-    // }
-
-    let sec = core::time::Duration::from_secs(1);
-
     handle.set_auto_detach_kernel_driver(true).unwrap();
-    handle.claim_interface(0x01).unwrap();
-    println!(
-        "{:#x?}",
-        handle.write_control(
-            0x21,   // bmRequestType for Set_Report
-            0x09,   // bRequest: SET_REPORT
-            0x0200, // wValue: Output (02), report id null
-            0x0001, // wIndex: interface id
-            &[0b00000111],
-            sec
-        )
-    );
+    handle.claim_interface(0x00).unwrap();
 
-    std::thread::sleep(sec);
-
-    // println!(
-    //     "{:#x?}",
-    //     handle.write_interrupt(
-    //         0x83,
-    //         &[0, 0,0,0,1,1,1,1],
-    //         core::time::Duration::from_secs(1)
-    //     )
-    // )
+    handle
+        .write_interrupt(0x02, &command_data, core::time::Duration::from_secs(1))
+        .unwrap();
 }
